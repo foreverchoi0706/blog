1515b141e14f1bdd6eaa6ee142598e241f2b4787
foreverChoi <foreverchoi0706@gmail.com>
2021-04-11 22:14:39 +0900

    project init

--- blog_front/pages/index.tsx
+++ blog_front/pages/index.tsx
@@ -1,5 +1,6 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import Link from "next/link";
+import Layout from "../components/Layout";
+import "tailwindcss/tailwind.css";
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
@@ -8,8 +9,11 @@ const IndexPage = () => (
       <Link href="/about">
         <a>About</a>
       </Link>
+      <button className="py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-green-500 hover:bg-green-700">
+        Hello, Tailwind CSS!
+      </button>
     </p>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
